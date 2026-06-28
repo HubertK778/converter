9aaa09370514e8e28a81dca0855846ca26cfb177
HubertK778 <khubert778@gmail.com>
2026-06-28 11:37:16 +0200

    feat: save to json file

--- converter.py
+++ converter.py
@@ -2,6 +2,31 @@ import argparse
 import sys
 import os
 import json
+from pathlib import Path
+
+def loadSourceFile(sourcePath, sourceExtension):
+    try:
+        with open(sourcePath, 'r', encoding='utf-8') as sourceFile:
+            content = sourceFile.read()
+
+            if sourceExtension == '.json':
+                content = json.loads(content)
+                return content
+    except json.JSONDecodeError as error:
+        print(f"Error: Invalid JSON format in {sourcePath}.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Error: File not found - {sourcePath}.")
+        sys.exit(1)
+
+def convertToFile(sourceContent, targetPath, targetExtension):
+    try:
+        with open(targetPath, 'w', encoding='utf-8') as targetFile:
+            if targetExtension == '.json':
+                json.dump(sourceContent, targetFile)
+    except Exception as error:
+        print(f"Error: Could not write to {targetPath}. {error}")
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -23,18 +48,12 @@ def main():
     args = parser.parse_args()
 
     sourcePath = args.file1path
+    sourceExtension = Path(sourcePath).suffix.lower()
+    
+    sourceContent = loadSourceFile(sourcePath, sourceExtension)
 
-    try:
-        with open(sourcePath, 'r', encoding='utf-8') as sourceFile:
-            _, fileExtension = os.path.splitext(sourcePath)
-            content = sourceFile.read()
+    targetPath = args.file2path
+    targetExtension = Path(targetPath).suffix.lower()
+    convertToFile(sourceContent, targetPath, targetExtension)
 
-            if fileExtension.lower() == '.json':
-                content = json.loads(content)
-    except json.JSONDecodeError as error:
-        print(f"Error: Invalid JSON format in {sourcePath}.")
-        sys.exit(1)
-    except FileNotFoundError:
-        print(f"Error: File not found - {sourcePath}.")
-        sys.exit(1)
 main()
