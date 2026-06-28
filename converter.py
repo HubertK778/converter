256ab90aa60798b3f58314998a172356be2e4254
HubertK778 <khubert778@gmail.com>
2026-06-28 11:28:54 +0200

    feat: add json parsing

--- converter.py
+++ converter.py
@@ -1,11 +1,13 @@
 import argparse
 import sys
+import os
+import json
 
 def main():
     parser = argparse.ArgumentParser(
         description="Skrypt przyjmujący dwie ścieżki do plików jako argumenty."
     )
-    
+
     parser.add_argument(
         "file1path", 
         type=str, 
@@ -20,7 +22,19 @@ def main():
 
     args = parser.parse_args()
 
-    print(f"Source file path: {args.file1path}")
-    print(f"Path to the second file: {args.file2path}")
+    sourcePath = args.file1path
 
+    try:
+        with open(sourcePath, 'r', encoding='utf-8') as sourceFile:
+            _, fileExtension = os.path.splitext(sourcePath)
+            content = sourceFile.read()
+
+            if fileExtension.lower() == '.json':
+                content = json.loads(content)
+    except json.JSONDecodeError as error:
+        print(f"Error: Invalid JSON format in {sourcePath}.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Error: File not found - {sourcePath}.")
+        sys.exit(1)
 main()
